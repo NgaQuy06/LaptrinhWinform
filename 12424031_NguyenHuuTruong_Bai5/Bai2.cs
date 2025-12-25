dc8ba3e132e1ca15c72459bea9d645a79b6ab25d
NgaQuy06 <cubietgay@gmail.com>
2025-12-25 08:08:15 +0700

    Add files via upload

--- 12424031_NguyenHuuTruong_Bai5/Bai2.cs
+++ 12424031_NguyenHuuTruong_Bai5/Bai2.cs
@@ -22,13 +22,19 @@ namespace BaiThucHanh5
         private void Bai2_Load(object sender, EventArgs e)
         {
             db = new QLSV1Entities1();
-            var sv = db.Sinhviens.ToList();
+            dgv.Rows.Clear();
+            var sv = from s in db.Sinhviens
+                     join bd in db.Bangdiems on s.MaSV equals bd.Masv
+                     select new { s.MaSV, s.Hoten, s.Ngaysinh, bd.DiemL1 };
             foreach (var s in sv)
             {
                 string hosv = s.Hoten.Split(' ')[0];
-                string tenSV = s.Hoten.Split(' ')[1] + " " + s.Hoten.Split(' ')[2];
-                var diem = db.Bangdiems.Where(masv => masv.Masv == s.MaSV).Select(dieml1 => dieml1.DiemL1);
-                dgv.Rows.Add(s.MaSV, hosv, tenSV, s.Ngaysinh, diem.FirstOrDefault());
+                string tenSV = s.Hoten.Split(' ')[1];
+                if (s.Hoten.Split(' ').Length > 2)
+                {
+                    tenSV = s.Hoten.Split(' ')[2];
+                }
+                dgv.Rows.Add(s.MaSV, hosv, tenSV, s.Ngaysinh, s.DiemL1);
             }
             LoadCboMaMH();
         }
@@ -36,22 +42,29 @@ namespace BaiThucHanh5
         private void LoadCboMaMH()
         {
             var mamh = db.Monhocs.Select(m => m.MaMH);
-            cboMaMH.DataSource = mamh.ToList();
-            cboMaMH.SelectedIndex = -1;
+            foreach (var m in mamh)
+            {
+                cboMaMH.Items.Add(m);
+            }
         }
 
         private void cboMaMH_SelectedIndexChanged(object sender, EventArgs e)
         {
             if (cboMaMH.SelectedIndex == -1) return;
-            var sv = db.Sinhviens.ToList();
             dgv.Rows.Clear();
-            foreach (var s in sv)
+            var sinhvien = from sv in db.Sinhviens
+                           join bd in db.Bangdiems on sv.MaSV equals bd.Masv
+                           join mh in db.Monhocs on bd.MaMh equals mh.MaMH
+                           where mh.MaMH == cboMaMH.SelectedItem.ToString()
+                           select new { sv.MaSV, sv.Hoten, sv.Ngaysinh, bd.DiemL1, mh.TenMH, mh.sotc };
+            foreach (var s in sinhvien)
             {
                 string hosv = s.Hoten.Split(' ')[0];
                 string tenSV = s.Hoten.Split(' ')[1] + " " + s.Hoten.Split(' ')[2];
-                var diem = db.Bangdiems.Where(d => d.MaMh == cboMaMH.SelectedItem.ToString() && d.Masv == s.MaSV);
-                dgv.Rows.Add(s.MaSV, hosv, tenSV, s.Ngaysinh, diem.FirstOrDefault());
+                dgv.Rows.Add(s.MaSV, hosv, tenSV, s.Ngaysinh, s.DiemL1);
             }
+            txtTenMH.Text = sinhvien.FirstOrDefault().TenMH;
+            txtSoTiet.Text = (int.Parse(sinhvien.FirstOrDefault().sotc.ToString()) * 15).ToString();
         }
 
         private void btnThoat_Click(object sender, EventArgs e)
